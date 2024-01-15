235e2503f07ac00aae063d54397d1a58c4b9b497
*360 <ehsanullahcs04@gmail.com>
2024-01-15 14:45:42 +0500

    Week Two

---
+++ week2/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    system("color 04");
+    cout << "        :::::::::::        " << endl;
+    cout << "       :::       :::       " << endl;
+    cout << "      ::           ::      " << endl;
+    cout << "     ::             ::     " << endl;
+    cout << "     :               :     " << endl;
+    cout << "     :               :     " << endl;
+    cout << "     ::             ::     " << endl;
+    cout << "      ::           ::      " << endl;
+    cout << "       :::       :::       " << endl;
+    cout << "        :::::::::::        " << endl;
+
+    return 0;
+}
